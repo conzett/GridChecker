5de39d9e79db643169a7a82cd177ba5d17073a8e
Graham Conzett <conzett@gmail.com>
2012-02-02 17:55:23 -0500

    Initial commit

---
+++ grid-checker.js
@@ -0,0 +1,75 @@
+var gridChecker = function (ColumnWidth, ColumnMargin, LineHeight, InnerPadding, Target) {
+	'use strict';
+
+	var container,
+		column,
+		inner,
+		line,
+		s,
+		i,
+		t,
+		columnNumber,
+		lineNumber,
+		target = document.getElementById(Target) || document.getElementsByTagName('body')[0];
+
+	columnNumber = parseInt(target.offsetWidth / (ColumnWidth + ColumnMargin), 10);
+	lineNumber = parseInt(window.innerHeight / LineHeight, 10);
+
+	s = 'height: 100%;';
+	s += 'position: absolute;';
+	s += 'top: 0;';
+	s += 'background-color: white;';
+	s += 'opacity: .3;';
+	s += 'filter:alpha(opacity=30);';
+	s += 'overflow: hidden';
+
+	container = document.createElement('div');
+	container.setAttribute('id', 'grid-checker');
+	container.setAttribute('style', s);
+
+	s = 'margin: 0 ' + InnerPadding + 'px;';
+	s += 'background-color: Gray;';
+	s += 'height: 100%;';
+	s += 'opacity: .4;';
+	s += 'filter:alpha(opacity=40);';
+
+	inner = document.createElement('div');
+	inner.setAttribute('class', 'gc-inner');
+	inner.setAttribute('style', s);
+
+	s = 'width: ' + ColumnWidth + 'px;';
+	s += 'margin: 0 ' + ColumnMargin + 'px;';
+	s += 'height: 100%;';
+	s += 'background-color: pink;';
+	s += 'float: left;';
+
+	column = document.createElement('div');
+	column.className = "gc-column";
+	column.setAttribute('style', s);
+	column.appendChild(inner.cloneNode(true));
+
+	for (i = 0; i < columnNumber; i += 1) {
+		container.appendChild(column.cloneNode(true));
+	}
+
+	s = "position: absolute;";
+	s += 'width: 100%;';
+	s += 'height: ' + LineHeight + 'px;';
+	s += 'z-index: 90;';
+	s += 'opacity: .2;';
+	s += 'filter:alpha(opacity=20);';
+
+	for (i = 0; i < lineNumber; i += 1) {
+		line = document.createElement('div');
+		line.className = "gc-line";
+		t = s;
+		t += 'top: ' + (i * LineHeight) + 'px;';
+		t += 'background-color: ';
+		t += (i % 2 === 0) ? 'blue' : 'white';
+		t += ';';
+		line.setAttribute('style', t);
+		container.appendChild(line.cloneNode(true));
+	}
+
+	target.appendChild(container);
+};
---
+++ tests/tests.js
@@ -0,0 +1,73 @@
+var containerID = "grid-checker",
+    fixtureID = "qunit-fixture",
+    columnClass = "gc-column";
+
+module("Main", {
+    setup: function () {
+        'use strict';
+    },
+    teardown: function () {
+        'use strict';
+
+        /*  Clean up container elements that 
+            might have been appended outside
+            of the fixture */
+
+        var element = document.getElementById(containerID);
+        element.parentNode.removeChild(element);
+    }
+});
+
+test("Grid checker element is generated.", function () {
+    'use strict';
+    gridChecker();
+    var element = document.getElementById(containerID);
+    ok(element, "Expect the " + containerID + " element to exist.");
+});
+
+test("Grid checker element is appended to specified target.", function () {
+    'use strict';
+    gridChecker(0, 0, 0, 0, fixtureID);
+    var element = document.getElementById(containerID);
+    equals(element.parentNode.getAttribute('id'), fixtureID, "Expect the ID of the parent to be " + fixtureID);
+});
+
+test("Generated width of columns is correct.", function () {
+    'use strict';
+    var testWidth = 102,
+        actuWidth,
+        element;
+    gridChecker(testWidth, 0, 0, 0, fixtureID);
+    element = document.getElementsByClassName(columnClass)[0];
+    actuWidth = (element) ? element.style.width : '0px';
+    equals(actuWidth, testWidth + "px", "Expect the width of the column to be " + testWidth);
+});
+
+test("Generated width of columns plus margins is correct.", function () {
+    'use strict';
+    var testWidth = 102,
+        testMargin = 3,
+        offsetWidth = 108,
+        actuWidth,
+        element;
+    gridChecker(testWidth, testMargin, 0, 0, fixtureID);
+    element = document.getElementsByClassName(columnClass)[0];
+    actuWidth = (element) ? element.offsetWidth : 0;
+    actuWidth += parseInt(element.style.marginRight, 10) + parseInt(element.style.marginLeft, 10);
+    equals(actuWidth, offsetWidth, "Expect the width of the column plus margin to be " + offsetWidth);
+});
+
+test("Correct number of columns generated for target width", function () {
+    'use strict';
+    var testWidth = 102,
+        testMargin = 3,
+        columns,
+        fixture;
+
+    fixture = document.getElementById(fixtureID);
+    fixture.setAttribute('style', 'width:550px');
+
+    gridChecker(testWidth, testMargin, 0, 0, fixtureID);
+    columns = document.getElementsByClassName(columnClass);
+    equals(columns.length, 5, "Expect 5 columns for a container of width 550px");
+});
